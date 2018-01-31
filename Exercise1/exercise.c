f4eed52d75722d71066ccb9b013def82fd5120d1
Kendall González <kendall.webs.tl@gmail.com>
2018-01-31 11:48:19 -0600

    Structure finished

--- Exercise1/exercise.c
+++ Exercise1/exercise.c
@@ -17,44 +17,27 @@ int main(int argc, char *argv[]){
 
   //logic
 
-  int counter = 0;
 
-  while (counter <= 1000) {
-
-    //send
-
-    if (rank == 0) {
+  if (rank == 0){
+    int counter = 0;
+    while (counter < 999){
       MPI_Send(&counter, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    }
-
-    else if( rank == 1 && counter != 1000) {
-      MPI_Send(&counter, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
-    }
-
-    else {
-      //This should not happend!
-    }
-
-    // Receive Data
-    // int MPI_Recv(void *buf, int count, MPI_Datatype datatype,int source, int tag, MPI_Comm comm, MPI_Status *status)
-    if (rank == 0) {
       MPI_Recv(&counter, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       counter += 1;
+      printf("Count value: %d on Rank: %d\n",counter+1, rank);
     }
+  }
 
-    else if( rank == 1) {
+  if (rank == 1){
+    int counter = 0;
+    while (counter < 998){
       MPI_Recv(&counter, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    printf("Counter : %d, rank: %d\n", counter,rank);
-
-    if(counter == 1000){
-      break;
+      MPI_Send(&counter, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+      printf("Count value: %d on Rank: %d\n",counter+1, rank);
     }
   }
 
   MPI_Finalize();
-
 
   return 0;
 }
